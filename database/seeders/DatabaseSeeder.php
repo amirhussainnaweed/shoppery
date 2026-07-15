e692c63a98d8c39b1f4a30c99e9c14f6cbe494eb
Noorulhaq Rahimi <noorulhaqrahimi11@gmail.com>
2026-07-15 13:48:00 -0400

    edit: any changes for factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Product;
 use App\Models\Order;
 use App\Models\Review;
+use App\Models\ShippingAddress;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,8 +24,9 @@ class DatabaseSeeder extends Seeder
         // SECOND: Create users with their relationships
         User::factory(200)
             ->has(\App\Models\Address::factory()->count(2))
+            ->has(\App\Models\ShippingAddress::factory()->count(2))
             ->has(\App\Models\Billing::factory()->count(2))
-            ->has(\App\Models\Cart::factory())
+            ->has(\App\Models\Cart::factory()->has(\App\Models\CartItem::factory()->count(3), 'cartItem'))
             ->has(\App\Models\Wishlist::factory()->count(3))
             ->has(\App\Models\Order::factory()->count(2))
             ->create();
@@ -33,7 +35,7 @@ class DatabaseSeeder extends Seeder
       Product::factory(50)
     ->has(\App\Models\ProductAttribute::factory()->count(3), 'productAttributes')
     ->has(\App\Models\ProductImage::factory()->count(2), 'images')
-    ->has(\App\Models\ProductTag::factory()->count(2), 'tags')
+    // ->has(\App\Models\ProductTag::factory()->count(2), 'tags')
     ->has(\App\Models\Review::factory()->count(5), 'reviews')
     ->create();
 
