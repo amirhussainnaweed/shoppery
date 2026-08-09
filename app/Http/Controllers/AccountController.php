a6f3c998628ab94665552baf28be170fd469c496
amirhussainnaweed <amirhussainnaweed@gmail.com>
2026-08-09 16:59:39 +0430

    feat: added billing address backend integration and created update function

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -9,10 +9,15 @@ class AccountController extends Controller
 {
     public function index(){
         $user = auth()->user();
+        $addresses = $user->addresses()->first();
         
         return Inertia::render('Account/Settings', [
-            'user' => $user
+            'user' => $user,
+            'addresses' => $addresses
         ]);
+
+
+        
     }
 
     public function update(Request $request){
@@ -33,6 +38,36 @@ class AccountController extends Controller
         $user->profile_image = $data['image'];
 
         $user->save();
+
+        return back();
+    }
+
+    public function updateBilling(Request $request){
+        $data = $request->validate([
+            'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'street' => ['required', 'string', 'max:255'],
+            'country' => ['required', 'string', 'max:255'],
+            'state' => ['required', 'string', 'max:255'],
+            'zipCode' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+        ]);
+
+        $user = auth()->user();
+        $addresses = $user->addresses()->first();
+
+        $user->name = $data['firstName'];
+        $user->lastname = $data['lastName'];
+        $user->email = $data['email'];
+        $user->phonenumber = $data['phone'];
+        $addresses->address_text = $data['street'];
+        $addresses->country = $data['country'];
+        $addresses->city = $data['state'];
+        $addresses->postal_code = $data['zipCode'];
+
+        $user->save();
+        $addresses->save();
 
         return back();
     }
