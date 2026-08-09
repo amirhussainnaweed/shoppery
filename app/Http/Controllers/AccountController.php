097260b1eb81964acda5e32c529532f341612859
amirhussainnaweed <amirhussainnaweed@gmail.com>
2026-08-09 11:51:24 +0430

    feat: update funtion added to accountcontroller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -14,4 +14,26 @@ class AccountController extends Controller
             'user' => $user
         ]);
     }
+
+    public function update(Request $request){
+        $data = $request->validate([
+            'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'image' => ['nullable', 'string', 'max:255'],
+        ]);
+
+        $user = auth()->user();
+
+        $user->name = $data['firstName'];
+        $user->lastname = $data['lastName'];
+        $user->email = $data['email'];
+        $user->phonenumber = $data['phone'];
+        $user->profile_image = $data['image'];
+
+        $user->save();
+
+        return back();
+    }
 }
